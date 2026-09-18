2300c4d3f74ea130a6cbfd9d736e99c8f8321d72
Junior Curvelo Santana <curvelojr@gmail.com>
2026-09-18 11:48:11 -0300

    Código base
    Código base

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'rildon-express-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Instalação: baixa os arquivos essenciais para o cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Ativação: remove caches antigos caso haja atualizações
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Interceptação de requisições: Cache-First com fallback de rede
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        // Se estiver offline e requisitando uma página HTML, entrega a raiz
+        if (event.request.headers.get('accept')?.includes('text/html')) {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
